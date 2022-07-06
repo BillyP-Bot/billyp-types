cf6aed2d9bb16a4580f0fab972076749d0974cee
Chris Leveille <cdleveille@gmail.com>
2022-07-05 22:15:24 -0400

    added connect four to gambling metrics

--- src/models/generics.ts
+++ src/models/generics.ts
@@ -64,6 +64,14 @@ export interface IGamblingMetrics {
 		overall_winnings: number;
 		overall_losings: number;
 	};
+	connect_four: {
+		games: number;
+		wins: number;
+		losses: number;
+		draws: number;
+		overall_winnings: number;
+		overall_losings: number;
+	};
 }
 
 export interface ILotteryMetrics {
